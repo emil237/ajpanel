18e0b9e05e1413b5a8b34e90f896e234f02f894d
emil237 <marianatalaat1@gmail.com>
2022-10-11 19:35:41 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -3,12 +3,12 @@
 #wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/ajpanel/main/installer.sh -O - | /bin/sh
 
 ##########################################
-version=7.2.0
+version=7.3.0
 #############################################################
 TEMPATH=/tmp
 OPKGINSTALL="opkg install --force-overwrite"
-MY_IPK="enigma2-plugin-extensions-ajpanel_v7.2.0_all.ipk"
-MY_DEB="enigma2-plugin-extensions-ajpanel_v7.2.0_all.deb"
+MY_IPK="enigma2-plugin-extensions-ajpanel_v7.3.0_all.ipk"
+MY_DEB="enigma2-plugin-extensions-ajpanel_v7.3.0_all.deb"
 MY_URL="https://raw.githubusercontent.com/emil237/ajpanel/main"
 # remove old version #
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AJPan
