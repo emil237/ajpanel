06c3decbc2307a8b56d735b636833cce60bc6263
emil237 <marianatalaat1@gmail.com>
2023-12-29 20:48:15 +0200

    Add files via upload

--- Ajpanel-menu.sh
+++ Ajpanel-menu.sh
@@ -1,8 +1,18 @@
-#!/bin/sh # ============================================================================================================ # SCRIPT : Download/Install AJPanel Custom-Menu XML Files # ============================================================================================================ 
-BACKUP_DIR=$(cat /etc/enigma2/settings | grep config.plugins.AJPanel.backupPath | cut -d '=' -f 
-2) for FNAME in "ajpanel_menu_biko_73.xml" "ajpanel_menu_Emil.xml" "ajpanel_menu_Haitham.xml" "ajpanel_menu.xml" do 	echo "--------------------------------------------------" 	echo ">>>>> Downloading $FNAME <<<<<" 	wget --no-check-certificate -O $BACKUP_DIR$FNAME https://raw.githubusercontent.com/emil237/ajpanel/main/$FNAME 	
-echo '' 
+#!/bin/sh
+# ============================================================================================================
+# SCRIPT : Download/Install AJPanel Custom-Menu XML Files
+# ============================================================================================================
+
+BACKUP_DIR=$(cat /etc/enigma2/settings | grep config.plugins.AJPanel.backupPath | cut -d '=' -f 2)
+
+for FNAME in "ajpanel_menu_biko_73.xml"  "ajpanel_menu_Emil.xml"  "ajpanel_menu_Haitham.xml"  "ajpanel_menu.xml"
+do
+	echo "--------------------------------------------------"
+	echo ">>>>>  Downloading $FNAME  <<<<<"
+	wget --no-check-certificate -O $BACKUP_DIR$FNAME https://raw.githubusercontent.com/emil237/ajpanel/main/$FNAME
+	echo ''
 done
+
 
 
 
