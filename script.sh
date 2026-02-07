5baec8cda004b7c106e40991ba5f32f11b9aaec8
emil237 <marianatalaat1@gmail.com>
2026-02-07 13:36:51 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
+##setup command=wget https://raw.githubusercontent.com/emil237/ajpanel/refs/heads/main/script.sh -O - | /bin/sh
 echo "Downloading scripts..."
 
 cd /tmp || exit 1
 
 wget -q --no-check-certificate \
-https://github.com/emil237/ajpanel/raw/main/script.tar.gz \
+https://github.com/emil237/ajpanel/raw/refs/heads/main/script.tar.gz \
 -O script.tar.gz
 
 tar -xzf script.tar.gz -C /
